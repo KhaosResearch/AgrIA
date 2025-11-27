28ffc4dfc5c2705678dee68704d539892206bd43
MiguelPadillaR <m.padi.r@icloud.com>
2025-11-27 12:14:23 +0000

    Feat: Ordered env vars + Added Docker env vars. + Chore: Grouped env vars in logic

--- AgrIA_server/run.py
+++ AgrIA_server/run.py
@@ -1,6 +1,7 @@
 from server import create_app
+from server.config.env_config import API_HOST, API_PORT
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=API_HOST ,port=API_PORT,debug=True)
