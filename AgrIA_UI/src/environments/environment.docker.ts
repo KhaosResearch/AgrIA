5e719a76f8e8e3e3410edd4240cff2142fa2a6d2
MiguelPadillaR <m.padi.r@icloud.com>
2025-11-27 13:15:04 +0000

    Fix: Finally fixed FE docker issues (Added ARG + Updated JSON config)

--- AgrIA_UI/src/environments/environment.docker.ts
+++ AgrIA_UI/src/environments/environment.docker.ts
@@ -4,5 +4,5 @@ export const environment: IEnvironment = {
   production: true,
   isDocker: true,
   apiPort: 5000, // Standard HTTPS port for production
-  apiUrl: 'http://backend:5000', // Your Docker API URL
+  apiUrl: '/api/', // Your Docker API URL
 };
